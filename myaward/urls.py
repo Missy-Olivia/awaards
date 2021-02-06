bb6b62d06599ec45bb587a02f68937f55c778cbc
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-02-06 18:55:04 +0200

    updated templates

--- myaward/urls.py
+++ myaward/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.contrib.auth import views
 
 urlpatterns = [
-    url(r'^admin', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'',include('award.urls')),
 ]
