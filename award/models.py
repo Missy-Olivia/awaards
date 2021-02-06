c80188a37053a85c162a9beb1f1738fa127f67ff
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-02-06 14:35:14 +0200

    added profile model

--- award/models.py
+++ award/models.py
@@ -23,3 +23,11 @@ class Profile(models.Model):
     def update_bio(cls,id, bio):
         update_profile = cls.objects.filter(id = id).update(bio = bio)
         return update_profile
+
+@classmethod
+    def get_all_profiles(cls):
+        profile = Profile.objects.all()
+        return profile
+    @classmethod
+    def search_user(cls,user):
+        return cls.objects.filter(user__username__icontains=user).all()
