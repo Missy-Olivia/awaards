2d843f24541e53b8b0bd1cde08fee76b7cc0f543
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-02-06 14:17:10 +0200

    added 1st view

--- award/views.py
+++ award/views.py
@@ -1,3 +1,27 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            email = form.cleaned_data.get('email')
+            user = User.objects.get(username=username)
+            profile=Profile.objects.create(user=user,email=email)
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = RegForm()
+    return render(request,'registration/registration_form.html')
