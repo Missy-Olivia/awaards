38bf4624e7015f182584bd8d1cd06f448b430400
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-02-08 02:13:50 +0200

    updated

--- award/urls.py
+++ award/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^accounts/profile/', views.my_profile, name='my_profile'),
-    url(r'accounts/register/',views.register, name='register'),
+    url(r'register/',views.register, name='register'),
     url(r'project/(\d+)',views.rate_project,name='rate-project'),
     url(r'profile/(\d+)',views.profile,name='profile'),
     url(r'my_profile',views.my_profile,name='my_profile'), 
